b23958de2d171200e0d64b9958b33872f2ef5483
yhc5091 <yhc5091@gmail.com>
2022-08-09 23:07:24 +0900

    fix comment

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 
-#include "solutions/programmers_42576.hpp"
+#include "solutions/programmers_42583.hpp"
 
 int main() {
-    programmers_42576::execute();
+    programmers_42583::execute();
 
     return 0;
 }
--- cpp/solutions/programmers_42583.hpp
+++ cpp/solutions/programmers_42583.hpp
@@ -1,4 +1,4 @@
-// https://school.programmers.co.kr/learn/courses/30/lessons/42576
+// https://school.programmers.co.kr/learn/courses/30/lessons/42583
 
 #include <string>
 #include <vector>
